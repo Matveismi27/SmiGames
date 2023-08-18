8f3a1ade19e753b70742126ea1a6ef9dcaf85568
Matveismi <matvej150@gmail.com>
2023-08-18 22:05:02 +0300

    patch 7

--- 9/script.js
+++ 9/script.js
@@ -1,16 +1,16 @@
 const canvas = document.getElementById("canvas")
 const ctx = canvas.getContext("2d")
 //нарисуем сетку
-let width = canvas.width-10
-let height = canvas.height-10
+let width = canvas.width
+let height = canvas.height
 // alert(width+" : "+height)
-let grid_rows = 20
-let grid_columns = 20
+let grid_rows = 7
+let grid_columns = 13
 let pole = [[]]
 let row = [[]]
-let bold = 4
+let bold = 2
 tictac = "x"
-function drawGrid(){
+function FirstDrawGrid(){
     for (i=0;i<=grid_columns;i+=1){
         ctx.fillRect(width/grid_columns*i,0              ,bold,height)
         row[i]=0
@@ -23,20 +23,36 @@ function drawGrid(){
         }
     }
 }
-drawGrid()
+function drawGrid(){
+    for (i=0;i<=grid_columns;i+=1){
+        ctx.fillRect(width/grid_columns*i,0              ,bold,height)
+    }
+    for (i=0;i<=grid_rows;i+=1){
+        ctx.fillRect(0                   ,height/grid_rows*i,width+bold,bold)
+    }
+}
+
+function drawGridRect(x1,y1,x2,y2,text){
+    for (i=x1;i<x2;i++){
+        for(j=y1;j<y2;j++){
+            pole[i][j]=text
+        }
+    }
+}
+FirstDrawGrid()
+drawGridRect(1,1,6,6,1)
+pole[1][1]=2
 function click(x,y){// функция производит необходимые действие при клике(касанию)
     console.log(pole)
-    if (y>grid_rows||x>grid_columns||pole[y][x]!=0){
+    if (y>grid_rows||x>grid_columns||pole[y][x]==0){
         return
     }
     console.log("клик на "+x+":"+y)
-    if (tictac == "x"){
-        pole[y][x] = 1; tictac = "o"
-    }
-    else{
-        pole[y][x] = 2; tictac = "x"
-    }
+    player=find(2)
+    pole[player.row][player.col]=1
+    pole[y][x]=2
     draw()
+    drawGrid()
 }
 canvas.onclick = function(e) { // обрабатываем клики мышью
     var x = (e.pageX - canvas.offsetLeft) / (width/grid_columns) | 0;//находит КЛЕТКУ!
@@ -44,19 +60,29 @@ canvas.onclick = function(e) { // обрабатываем клики мышью
     click(x, y); // выхов функции действия
 };
 
+function find(text){
+    for (i in pole){
+        for (j in pole[i]){
+            if (pole[i][j] == text){
+                return {row:i,col:j}
+            }
+        }
+    }
+}
+
 function draw(){
     i=0
     j=0
     while (j<grid_rows){
         while (i<grid_columns){
             if (pole[j][i]==0){
-                ctx.fillStyle = "#ddd"
+                ctx.fillStyle = "#000"
             }else if (pole[j][i]==1){
-                ctx.fillStyle = "#900"
+                ctx.fillStyle = "#fff"
             }else if (pole[j][i]==2){
-                ctx.fillStyle = "#009"
+                ctx.fillStyle = "#999"
             }
-            ctx.fillRect(i*(width/grid_columns)+bold,j*(height/grid_rows)+bold,width/grid_columns-bold,height/grid_rows-bold)
+            ctx.fillRect(i*(width/grid_columns)+bold,j*(height/grid_rows)+bold,width/grid_columns,height/grid_rows)
             i+=1
         }
         i=0
@@ -64,3 +90,4 @@ function draw(){
     }
 }
 draw()
+drawGrid()
